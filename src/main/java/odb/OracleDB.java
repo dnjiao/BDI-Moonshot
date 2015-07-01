79363bc1f7a3b91138ed16e69dbc6f481a0ea7a2
dnjiao <djiao@RISTMCL8009547.local>
2015-07-01 15:14:13 -0500

    connection throws exception

--- src/main/java/odb/OracleDB.java
+++ src/main/java/odb/OracleDB.java
@@ -2,23 +2,20 @@ package odb;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class OracleDB
 {
-	public Connection getConnection(String user, String pass)
+	public Connection getConnection() throws SQLException
 	{
-		try
-		{
-			String connectionURL = "ldap://mdaoid.mdanderson.org:389/risdev3, cn=OracleContext,dc=mdacc,dc=tmc,dc=edu";
-			Connection connection = null;
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			connection = DriverManager.getConnection(connectionURL, user, pass);
-			
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
+		
+		String connectionURL = "ldap://mdaoid.mdanderson.org:389/risdev3, cn=OracleContext,dc=mdacc,dc=tmc,dc=edu";
+		Connection connection = null;
+		String user = "BDI_USER";
+		String pass = "dbiuser123";
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		connection = DriverManager.getConnection(connectionURL, user, pass);
+		System.out.println("Connected to database");
 		return connection;
 	}
 }
