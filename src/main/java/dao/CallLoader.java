d064e696b638ce27b38b4e2c40b0cfa609574814
dnjiao <djiao@RISTMCL8009547.local>
2015-07-24 17:47:31 -0500

    main added for CallLoader

--- src/main/java/dao/CallLoader.java
+++ src/main/java/dao/CallLoader.java
@@ -4,21 +4,25 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Loaders {
-	public static void callLoader (String type) {
+public class CallLoader {
+	public static void main(String[] args) {
+		final String TYPE = System.getenv("TYPE").toLowerCase();
+		loader(TYPE);
+	}
+	public static void loader (String type) {
 		Connection conn = OracleDB.getConnection();
 		String[] arr = new String[6];
-		if (type.equalsIgnoreCase("vcf"))
+		if (type.equals("vcf"))
 			arr[0] = "Y";
-		if (type.equalsIgnoreCase("cnv"))
+		if (type.equals("cnv"))
 			arr[1] = "Y";
-		if (type.equalsIgnoreCase("rna"))
+		if (type.equals("rna"))
 			arr[2] = "Y";
-		if (type.equalsIgnoreCase("flowcyto"))
+		if (type.equals("flowcyto"))
 			arr[3] = "Y";
-		if (type.equalsIgnoreCase("immunopath"))
+		if (type.equals("immunopath"))
 			arr[4] = "Y";
-		if (type.equalsIgnoreCase("mapping"))
+		if (type.equals("mapping"))
 			arr[5] = "Y";
 		try {
 			CallableStatement pstmt = conn.prepareCall("{call FILE_LOADER.process_file(?,?,?,?,?,?)}");
