6fa3c39f23b4ca5e95f42d1964cdb1314026e0c9
dnjiao <djiao@RISTMCL8009547.local>
2015-08-11 15:10:28 -0500

    new url to post files

--- src/main/java/rest/PushFiles.java
+++ src/main/java/rest/PushFiles.java
@@ -22,7 +22,8 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 public class PushFiles {
-	final static String URL_STRING = "http://10.111.100.207:8098/bdi/serviceingestion?domain=";
+//	final static String URL_STRING = "http://10.111.100.207:8098/bdi/serviceingestion?domain=";
+	final static String URL_STRING = "http://10.113.241.42:8099/bdi/serviceingestion?domain=";
 	final static String LOCAL_PATH = "/rsrch1/rists/moonshot";
 	
 	public static void main(String[] args) {
